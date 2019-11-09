38eb29da8e4b47ceabcdcfed91d4a8333d83cd54
Surbhi Jain <surbhi.kmv@gmail.com>
2019-11-09 16:57:37 +0530

    first commit

---
+++ Surbhi_Nagp_MicroservicesAssignmen/WebApp/CustomerClient.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Options;
+using Steeltoe.CircuitBreaker.Hystrix;
+using System.Net.Http;
+
+namespace WebApp
+{
+    public class CustomerClient : HystrixCommand<string>
+    {
+        ConfigSettings configSettings { get; set; }
+
+        public CustomerClient(IHystrixCommandOptions options, IOptions<ConfigSettings> configSettings)
+            : base(options)
+        {
+            this.configSettings = configSettings.Value;
+        }
+
+        protected override string Run()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync("https://localhost:7002/api/values").Result;
+            var data = response.Content.ReadAsStringAsync().Result;
+            return data;
+        }
+
+        protected override string RunFallback()
+        {
+            return "Hey I am from fallback";
+        }
+    }
+}
---
+++ Surbhi_Nagp_MicroservicesAssignmen/WebApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((webHostBuilderContext, configurationBuilder) =>
+            {
+                var hostingEnvironment = webHostBuilderContext.HostingEnvironment;
+                configurationBuilder.AddConfigServer(hostingEnvironment.EnvironmentName);
+            })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
