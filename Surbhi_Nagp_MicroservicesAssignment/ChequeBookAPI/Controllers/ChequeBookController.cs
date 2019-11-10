ff19fb6a77ac99a24c0ff6e6453e9337748cfe52
Surbhi Jain <surbhi.kmv@gmail.com>
2019-11-10 19:46:11 +0530

    all

---
+++ Surbhi_Nagp_MicroservicesAssignment/ChequeBookAPI/Controllers/ChequeBookController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChequeBookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ChequeBookController : Controller
+    {
+        // GET api/ChequeBook
+        [HttpGet]
+        public string Get()
+        {
+            return "I am cheque book API";
+        }
+
+        // POST api/ChequeBook
+        [HttpPost]
+        public string OrderChequeBook([FromBody]string accountId)
+        {
+            return "Check book ordered";
+        }
+
+        // POST api/ChequeBook/34
+        [HttpPut("{chequeBookId}")]
+        public string BlockChequeBook(int chequeBookId, [FromBody]string accountId)
+        {
+            return "Check book blocked";
+        }
+    }
+}
